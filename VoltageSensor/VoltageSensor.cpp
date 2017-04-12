b6d95768aa13b51afbf58aa5c2919dad850fc058
xrgman <xrgman@gmail.com>
2017-04-12 12:42:26 +0200

    Added complete Voltage sensor

---
+++ VoltageSensor/VoltageSensor.cpp
@@ -0,0 +1,38 @@
+#include "VoltageSensor.h"
+
+/*!
+ * Constructor.
+ * @param pinNumber - The number of the pin that the sensor is connected to.
+ */
+VoltageSensor::VoltageSensor(int pinNumber) {
+    this->pinNumber = pinNumber;
+
+    //Setting default resistor values for module from ebay:
+    this->R1 = 30000.0;
+    this->R2 = 7400.0;
+}
+
+/*!
+ * Constructor, with ability to set different resistor values. Used for own made sensor.
+ * @param pinNumber- The number of the pin that the sensor is connected to.
+ * @param R1 - Value of the first resistor.
+ * @param R2 - Value of the first resistor.
+ */
+VoltageSensor::VoltageSensor(int pinNumber, float R1, float R2) {
+    this->pinNumber = pinNumber;
+    this->R1 = R1;
+    this->R2 = R2;
+}
+
+/*!
+ * ReadSensor
+ *
+ * Fetch the current voltage from the connected powersource.
+ * @return Voltage in (V) for the power source.
+ */
+float VoltageSensor::readSensor() {
+    float value = analogRead(pinNumber);
+    float vOut = (value * 5.0) / 1024.0;
+    float vIn = vOut / (R2/(R1+R2));
+    return vIn;
+}
---
+++ VoltageSensor/VoltageSensor.h
@@ -0,0 +1,15 @@
+#ifndef VOLTAGESENSOR_H
+#define VOLTAGESENSOR_H
+
+#include "arduino.h";
+
+class VoltageSensor {
+    public:
+        VoltageSensor(int);
+        VoltageSensor(int, float, float);
+        float readSensor();
+    private:
+        float R1, R2;
+        int pinNumber;
+};
+#endif //VOLTAGESENSOR_H
